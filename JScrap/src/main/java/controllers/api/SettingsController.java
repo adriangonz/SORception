529ed6656df75fbea4261ff10e4aa65ab998a301
Adrian Gonzalez <kaseyo23@gmail.com>
2013-10-28 21:09:37 +0100

    Me rindo con el mapeo automatico, se ponen las URLs a pelo

--- JScrap/src/main/java/controllers/api/SettingsController.java
+++ JScrap/src/main/java/controllers/api/SettingsController.java
@@ -1,21 +1,17 @@
-package controllers;
+package controllers.api;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/")
-public class IndexController {
-	private static final Logger LOG = LoggerFactory.getLogger(IndexController.class);
+@RequestMapping("/api/settings")
+public class SettingsController {
 	
 	@RequestMapping(value = {"","/"}, method=RequestMethod.GET)
 	@ResponseBody
 	public String get() {
-		LOG.debug("This should return some text...");
-		return "Ola ke ase!";
+		return "<h1>Prueba</h1>";
 	}
 }
