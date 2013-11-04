87c173069f7022c076147cdc3962092c3e7dd89b
wen96 <yosoyruben@gmail.com>
2013-11-04 12:18:57 +0100

    Ontro intento lamentable, esta vez de conectar directamente con la BD

---
+++ sg/SG/AdminManager/Controllers/DesguaceController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.EntityClient;
+
+namespace AdminManager.Controllers
+{
+    public class DesguaceController : ApiController
+    {
+        // GET api/desguace
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/desguace/5
+        public string Get(int id)
+        {
+            /*using (managersystemEntities ms_ent = new managersystemEntities())
+            {
+                var desguace = (from d in ms_ent.DesguaceConjunto
+                                where d.id == id
+                                select d).First();
+                desguace.active = true;
+                ms_ent.SaveChanges();
+            }*/
+            return "value";
+        }
+
+        // POST api/desguace
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/desguace/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/desguace/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
