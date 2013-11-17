960617fa011b1ad0b55f309233056925855d680e
wen96 <yosoyruben@gmail.com>
2013-11-17 17:32:47 +0100

    Merge bastante montanyoso pero ya: TODO SE CONVIRTIO EN CHOCAPIII

--- sg/SG/ManagerSystem/ManagerSystemEntityModel.edmx.sql
+++ sg/SG/ManagerSystem/ManagerSystemEntityModel.edmx.sql
@@ -2,7 +2,7 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, and Azure
 -- --------------------------------------------------
--- Date Created: 11/17/2013 17:04:23
+-- Date Created: 11/17/2013 17:17:07
 -- Generated from EDMX file: C:\Users\Ruben\Documents\sorception\sg\SG\ManagerSystem\ManagerSystemEntityModel.edmx
 -- --------------------------------------------------
 
