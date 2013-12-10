3b154da9cfe48436cdb32a3284ece932cfa139e0
wen96 <yosoyruben@gmail.com>
2013-12-10 15:46:14 +0100

    Berenjena: Creada la bd de tokens. Ya funcionan las url como pone en la wiki. Para cualquier duda al respecto consultarme.

--- taller/Berenjena/Berenjena/Controllers/GestionController.cs
+++ taller/Berenjena/Berenjena/Controllers/GestionController.cs
@@ -10,26 +10,43 @@ namespace Berenjena.Controllers
 {
     public class GestionController : ApiController
     {
-        // TODO: HAY QUE SACAR EL ID DE LAS COOKIES
-
+        BDBerenjenaContainer c_bd = new BDBerenjenaContainer();
         ServiceTaller.GestionTallerClient svcTaller = new ServiceTaller.GestionTallerClient();
+        string REQUESTED = "REQUESTED";
+        string EXPIRED = "EXPIRED";
+        string ACTIVE = "ACTIVE";
+        
         // GET api/gestion
         public object Get()
         {
-            ServiceTaller.Taller t = svcTaller.getTaller(1);
-            return (new { id= t.Id, name= t.name, active=t.active });
+            if (c_bd.TokensSet.Count() <= 0) return Request.CreateResponse(HttpStatusCode.InternalServerError, "No se ha solicitado token");
+            var token = (from d in c_bd.TokensSet orderby d.TimeStamp descending select d);
+
+            if (token.First().state != REQUESTED && token.First().state != EXPIRED)
+            {
+                ServiceTaller.Taller t = svcTaller.getTaller(token.First().token);
+                return (new { id = t.Id, name = t.name, active = t.active, Token = new { token } });
+            }
+            return Request.CreateResponse(HttpStatusCode.Forbidden, "El token esta expirado o no activado");
         }
 
         // POST api/gestion
         public void Post([FromBody]JObject value)
         {
-            svcTaller.addTaller(value["nombre"].ToString());
+            setLastTokenAsExpered();
+            Tokens to = new Tokens();
+            to.token = svcTaller.addTaller(value["nombre"].ToString());
+            to.TimeStamp = DateTime.Now;
+            to.state = REQUESTED;
+            c_bd.TokensSet.Add(to);
+            c_bd.SaveChanges();
         }
 
         // PUT api/gestion
         public void Put([FromBody]JObject value)
         {
-            ServiceTaller.Taller t = svcTaller.getTaller(1);
+            var token = (from d in c_bd.TokensSet orderby d.TimeStamp select d).First();
+            ServiceTaller.Taller t = svcTaller.getTaller(token.token);
             t.name = value["nombre"].ToString();
             svcTaller.putTaller(t);
         }
@@ -37,13 +54,49 @@ namespace Berenjena.Controllers
         // DELETE api/gestion
         public void Delete()
         {
-            svcTaller.deleteTaller(1);
+            var token = (from d in c_bd.TokensSet orderby d.TimeStamp select d).First();
+            svcTaller.deleteTaller(token.token);
         }
 
         // GET api/gestion/token
         [Route("api/gestion/token")]
-        public string GetToken() { 
-            return "asdfasdfTomaTokenInventao";
+        public object GetToken() {
+            if (c_bd.TokensSet.Count() <= 0) return Request.CreateResponse(HttpStatusCode.InternalServerError, "No se ha solicitado token");
+            var token = (from d in c_bd.TokensSet orderby d.TimeStamp descending select d).First();
+            // Si el token esta pendiente de activacion
+            if (token.state == REQUESTED)
+            {
+                int st = svcTaller.getState(token.token);
+                if (st == -1)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "El token no ha sido activado");
+                }
+                else
+                {
+                    token.state = ACTIVE;
+                    c_bd.SaveChanges();
+                    return (new { Token = token.token, Status = token.state, Created = token.TimeStamp });
+
+                }
+            }
+            else if (token.state == ACTIVE)
+            {
+                return (new { Token = token.token, Status = token.state, Created = token.TimeStamp });
+            }
+            return Request.CreateResponse(HttpStatusCode.Forbidden, "El token ha expirado");
+        }
+
+        private void setLastTokenAsExpered()
+        {
+            if (c_bd.TokensSet.Count() > 0)
+            {
+                var tokens = (from d in c_bd.TokensSet select d);
+                foreach (var token in tokens)
+                {
+                    token.state = EXPIRED;
+                }
+                c_bd.SaveChanges();
+            }
         }
     }
 }
