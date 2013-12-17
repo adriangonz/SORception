dd1e41dfa75caecbd28a8dc72c447d733bd1dcb8
wen96 <yosoyruben@gmail.com>
2013-12-17 13:46:46 +0100

    Implementado el registro en un proyecto aparte en la misma solucion. Cuando sepamos seguro que funciona bien bien borramos el de Berenjena y dejamos solo el de Eggplant. Esta implementado con tokens y tal como debe estar en una api, el tema de que siempre te lleve a una página de login y tal debe hacerse desde Angular. El tema de los roles no esta implementado. Queria esperar a ponerle a register el acceso limitado a que se manejara ya todo bien desde angular no sea cosa que deje sin acceso al registro.

---
+++ taller/Berenjena/Berenjena/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Berenjena.Controllers
+{
+    [Authorize]
+    public class AccountController : ApiController
+    {
+        // GET api/account
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/account/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/account
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/account/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/account/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ taller/Berenjena/Eggplant/App_Start/Startup.Auth.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using Eggplant.Providers;
+
+namespace Eggplant
+{
+    public partial class Startup
+    {
+        static Startup()
+        {
+            PublicClientId = "self";
+
+            UserManagerFactory = () => new UserManager<IdentityUser>(new UserStore<IdentityUser>());
+
+            OAuthOptions = new OAuthAuthorizationServerOptions
+            {
+                TokenEndpointPath = new PathString("/Token"),
+                Provider = new ApplicationOAuthProvider(PublicClientId, UserManagerFactory),
+                AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                AllowInsecureHttp = true
+            };
+        }
+
+        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+
+        public static Func<UserManager<IdentityUser>> UserManagerFactory { get; set; }
+
+        public static string PublicClientId { get; private set; }
+
+        // Para obtener más información para configurar la autenticación, visite http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Enable the application to use a cookie to store information for the signed in user
+            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Permitir que la aplicación use tokens portadores para autenticar usuarios
+            app.UseOAuthBearerTokens(OAuthOptions);
+
+            // Quitar los comentarios de las siguientes líneas para habilitar el inicio de sesión con proveedores de inicio de sesión de terceros
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //app.UseTwitterAuthentication(
+            //    consumerKey: "",
+            //    consumerSecret: "");
+
+            //app.UseFacebookAuthentication(
+            //    appId: "",
+            //    appSecret: "");
+
+            //app.UseGoogleAuthentication();
+        }
+    }
+}
