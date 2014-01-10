e40a9703460298e4e589fb2fe0bff339ea33ca4e
wen96 <yosoyruben@gmail.com>
2014-01-10 14:59:00 +0100

    Arreglos que codigo varios, ahora ya las solicitudes se envian con el token activado en la cabecera xml si existe si no sin token

--- taller/Berenjena/Eggplant/Filters/WCFAuthorizationHeader.cs
+++ taller/Berenjena/Eggplant/Filters/WCFAuthorizationHeader.cs
@@ -17,59 +17,20 @@ namespace Eggplant.Filters
         public object BeforeSendRequest(ref System.ServiceModel.Channels.Message request, System.ServiceModel.IClientChannel channel)
         {
 
-            /*using (BDBerenjenaContainer c_bd = new BDBerenjenaContainer())
-            {
-                Eggplant.ServiceTaller.GestionTallerClient svcTaller = new Eggplant.ServiceTaller.GestionTallerClient();
-                Tokens t = c_bd.TokensSet.AsQueryable().FirstOrDefault(x => x.state == "ACTIVE");
-                string token = "";
-                if (t != null) token = t.token;
-                using (OperationContextScope scope = new OperationContextScope(svcTaller.InnerChannel))
-                {
-                    MessageHeader header = MessageHeader.
-                        CreateHeader("Authorization", "http://sorception.azurewebsites.net/", token);
-                    OperationContext.Current.OutgoingMessageHeaders.Add(header);
-
-                }
-            }*/
-             MessageHeader header = MessageHeader.
-                        CreateHeader("Authorization", "http://sorception.azurewebsites.net/", "COCAINUM");
-             request.Headers.Add(header);
-            /*
-            HttpRequestMessageProperty httpRequestMessage;
-            object httpRequestMessageObject;
-            if (request.Properties.TryGetValue(HttpRequestMessageProperty.Name, out httpRequestMessageObject))
-            {
-                httpRequestMessage = httpRequestMessageObject as HttpRequestMessageProperty;
-                if (string.IsNullOrEmpty(httpRequestMessage.Headers["Authorization"]))
-                {
-                    httpRequestMessage.Headers["Authorization"] = "COAINUM";
-                }
-            }
-            else
-            {
-                httpRequestMessage = new HttpRequestMessageProperty();
-                httpRequestMessage.Headers.Add("Authorization", "COAINUM");
-                request.Properties.Add(HttpRequestMessageProperty.Name, httpRequestMessage);
-            }*/
-            return null;
-        }
-        /*
-        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
-        {
             using (BDBerenjenaContainer c_bd = new BDBerenjenaContainer())
             {
                 Eggplant.ServiceTaller.GestionTallerClient svcTaller = new Eggplant.ServiceTaller.GestionTallerClient();
                 Tokens t = c_bd.TokensSet.AsQueryable().FirstOrDefault(x => x.state == "ACTIVE");
-                string token = "";
-                if (t != null) token = t.token;
-                using (OperationContextScope scope = new OperationContextScope(svcTaller.InnerChannel))
+                if (t != null)
                 {
                     MessageHeader header = MessageHeader.
-                        CreateHeader("Authorization", "http://sorception.azurewebsites.net/", "token");
-                    OperationContext.Current.OutgoingMessageHeaders.Add(header);
-
+                           CreateHeader("Authorization", "http://sorception.azurewebsites.net/", t.token);
+                    request.Headers.Add(header);
                 }
+            
             }
-        }*/
+            return null;
+        }
+        
     }
 }
