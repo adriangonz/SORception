523362ebc16ff25f2acdcba799aeb9af703d18cc
wen96 <yosoyruben@gmail.com>
2014-01-14 21:27:18 +0100

    Solicitud: Ahora se guardan los faileds y se agrega la referencia desde el SG. Tambien ahora se devuelven los 404 en el put. Ofertas: Ya se devuelven pero no esta testeado porque no funciona sl SG Pedido: Creado el controler y comenzado a implementar en la bd

---
+++ taller/Berenjena/Eggplant/Controllers/PedidoController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Eggplant.ServiceTaller;
+
+namespace Eggplant.Controllers
+{
+    public class PedidoController : ApiController
+    {
+        static BDBerenjenaContainer c_bd = new BDBerenjenaContainer();
+        static Eggplant.ServiceTaller.GestionTallerClient svcTaller = new Eggplant.ServiceTaller.GestionTallerClient();
+
+        // GET api/pedido
+        public object Get()
+        {
+            TallerResponse tr = new TallerResponse();//TODO SG
+            TallerResponse.SelectedLine sl = new TallerResponse.SelectedLine();
+            sl.quantity = 5;//tr.selected_lines
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/pedido/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/pedido
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/pedido/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/pedido/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
