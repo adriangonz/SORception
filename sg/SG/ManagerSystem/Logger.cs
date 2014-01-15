9783c3ef2195bc8db1d0536d7fd481097f0df578
Martín Candela Calabuig <rellikiox@gmail.com>
2014-01-15 19:13:39 +0100

    Añadido Logger

---
+++ sg/SG/ManagerSystem/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManagerSystem
+{
+    public class Logger
+    {
+        static managersystemEntities ms_ent = new managersystemEntities();
+
+        public static void Log(string message, string level)
+        {
+            Log l = new Log();
+
+            l.level = level;
+            l.message = message;
+            l.timestamp = DateTime.Now;
+
+            ms_ent.Logs.Add(l);
+            ms_ent.SaveChanges();
+        }
+
+        public static void Info(string message)
+        {
+            Log(message, "INFO");
+        }
+
+        public static void Error(string message)
+        {
+            Log(message, "ERROR");
+        }
+    }
+}
