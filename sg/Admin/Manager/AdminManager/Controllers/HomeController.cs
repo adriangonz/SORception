d805f642218ac7923a5bd41d6d24030e1510d616
wen96 <yosoyruben@gmail.com>
2014-01-15 16:52:15 +0100

    INDEPENDENCIA PER ALS PAISOS CATALANS AMB PA A LA BOCA

---
+++ sg/Admin/Manager/AdminManager/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            ServiceAdmin.GestionAdminClient gd = new ServiceAdmin.GestionAdminClient();
+            var desguaces = gd.getDesguaces();
+            var talleres = gd.getTalleres();
+            ViewBag.Message = "Tenemos " + desguaces.Count() + " desguaces y  " + talleres.Count() + " talleres";
+
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Desguaces()
+        {
+            ServiceAdmin.GestionAdminClient gd = new ServiceAdmin.GestionAdminClient();
+            var desguaces = gd.getDesguaces();
+            return View(desguaces.ToList());
+        }
+
+        [Authorize]
+        public ActionResult Talleres()
+        {
+            ServiceAdmin.GestionAdminClient gd = new ServiceAdmin.GestionAdminClient();
+            var talleres = gd.getTalleres();
+            return View(talleres.ToList());
+        }
+    }
+}
