76529e25148f86d4920c373061d7a1eb5cb141d4
Adrián González <kaseyo23@gmail.com>
2014-01-19 20:39:37 +0100

    Añado comienzos de scrap.net

---
+++ ScrapWeb/ScrapWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,14 @@
+﻿using ScrapWeb.Filters;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScrapWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            //filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ScrapWeb/ScrapWeb/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using ScrapWeb.DTO;
+using ScrapWeb.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ScrapWeb.Controllers
+{
+    [RoutePrefix("api/user")]
+    public class UserController : ApiController
+    {
+        private UserService userService;
+
+        public UserController()
+        {
+            userService = new UserService();
+        }
+
+        // GET api/user
+        public IEnumerable<IdentityUser> Get()
+        {
+            var userStore = new UserStore<IdentityUser>();
+            return userStore.Context.Set<IdentityUser>();
+        }
+
+        // GET api/user/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/user
+        public UserInfoDTO Post(DTO.UserRegisterDTO user)
+        {
+            return userService.registerUser(user);
+        }
+
+        // DELETE api/user/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
