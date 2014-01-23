ac7973aa37a18d052409585c8e0e6209616994d4
Martín Candela Calabuig <rellikiox@gmail.com>
2014-01-23 01:35:35 +0100

    Creado OrderService.addOrder

--- sg/SG/ManagerSystem/Services/OrderService.cs
+++ sg/SG/ManagerSystem/Services/OrderService.cs
@@ -1,4 +1,5 @@
 ﻿using ManagerSystem.DataAccess;
+using ManagerSystem.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,20 @@ namespace ManagerSystem.Services
     {
         public OrderService(UnitOfWork uow = null) : base(uow) { }
 
+        public int addOrder(ExpSolicitud e_order)
+        {
+            OrderEntity order = new OrderEntity();
+
+            order.garage = garageService.getCurrentGarage();
+            order.deadline = e_order.deadline;
+            order.corresponding_id = e_order.id_en_taller;
+
+            unitOfWork.OrderRepository.Insert(order);
+            unitOfWork.Save();
+
+            return order.id;
+        }
+
         public ExpSolicitud getOrder(int order_id)
         {
             return new ExpSolicitud();
@@ -18,11 +33,6 @@ namespace ManagerSystem.Services
         public List<ExpSolicitud> getOrders()
         {
             return new List<ExpSolicitud>();
-        }
-
-        public int addOrder(ExpSolicitud e_order)
-        {
-
         }
 
         public void putOrder(ExpSolicitud e_order)
