5100f989765af79dbbf790f11c0bf57f1ec48183
wen96 <yosoyruben@gmail.com>
2014-02-02 18:29:49 +0100

    Taller.NET: Implementadas en code first las solicitudes y lineas de solicitud

--- taller/Berenjena/Eggplant/Entity/LineaSolicitud.cs
+++ taller/Berenjena/Eggplant/Entity/LineaSolicitud.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,23 @@ namespace Eggplant.Entity
 {
     public class LineaSolicitud : AbstractEntity
     {
+        public static string EFFECT_NEW = "NEW";
+        public static string EFFECT_UPDATED = "UPDATED";
+        public static string EFFECT_DELETE = "DELETED";
+        public static string EFFECT_NOEFECT = "NOEFECT";
+
+        public string descripcion { get; set; }
+        public int cantidad { get; set; }
+        public int sg_id { get; set; }
+        public string criterio { get; set; }
+        public string status { get; set; }
+
+        public int solicitudId { get; set; }
+
+        
+        [Required]
+        [ForeignKey("solicitudId")]
+        public virtual Solicitud solicitud { get; set; }
+        
     }
 }
--- taller/Berenjena/Eggplant/Entity/Solicitud.cs
+++ taller/Berenjena/Eggplant/Entity/Solicitud.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,27 @@ namespace Eggplant.Entity
 {
     public class Solicitud : AbstractEntity
     {
+        public static string DELETED = "DELETED";
+
+        public Solicitud()
+        {
+            rawLines = new List<LineaSolicitud>();
+        }
+
+        public int sg_id { get; set; }
+        public string status { get; set; }
+        public string user_id { get; set; }
+
+        
+        [JsonIgnore]
+        public ICollection<LineaSolicitud> rawLines { get; set; }
+
+        public virtual IEnumerable<LineaSolicitud> lines
+        {
+            get
+            {
+                return rawLines.Where(t => t.status != DELETED);
+            }
+        }
     }
 }
