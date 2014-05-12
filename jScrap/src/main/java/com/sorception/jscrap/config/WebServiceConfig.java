d3938a857412ba31ce5785c3012b70aaab61a938
Adrián González Martín <kaseyo23@gmail.com>
2014-05-12 16:57:53 +0200

    Ya funca el juddi

--- jScrap/src/main/java/com/sorception/jscrap/config/WebServiceConfig.java
+++ jScrap/src/main/java/com/sorception/jscrap/config/WebServiceConfig.java
@@ -7,6 +7,7 @@
 package com.sorception.jscrap.config;
 
 import org.apache.commons.configuration.ConfigurationException;
+import org.apache.juddi.v3.client.UDDIConstants;
 import org.apache.juddi.v3.client.config.UDDIClient;
 import org.apache.juddi.v3.client.transport.Transport;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,10 +17,13 @@ import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.ws.soap.SoapVersion;
 import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
+import org.uddi.api_v3.AuthToken;
 import org.uddi.api_v3.BindingDetail;
 import org.uddi.api_v3.BindingTemplate;
 import org.uddi.api_v3.BusinessService;
+import org.uddi.api_v3.FindQualifiers;
 import org.uddi.api_v3.FindService;
+import org.uddi.api_v3.GetAuthToken;
 import org.uddi.api_v3.GetBindingDetail;
 import org.uddi.api_v3.GetServiceDetail;
 import org.uddi.api_v3.Name;
@@ -27,6 +31,7 @@ import org.uddi.api_v3.ServiceDetail;
 import org.uddi.api_v3.ServiceInfo;
 import org.uddi.api_v3.ServiceList;
 import org.uddi.v3_service.UDDIInquiryPortType;
+import org.uddi.v3_service.UDDISecurityPortType;
 
 import com.sorception.jscrap.generated.ObjectFactory;
 
@@ -40,8 +45,11 @@ public class WebServiceConfig {
 	@Value("${webservice.serviceName}")
     private String serviceName;
 	
-	@Value("${webservice.inquiryUrl}")
-    private String inquiryUrl;
+	@Value("${webservice.juddiUser}")
+    private String juddiUser;
+	
+	@Value("${webservice.juddiPassword}")
+    private String juddiPassword;
 	
     @Value("${webservice.defaultUrl}")
     private String defaultUrl;
@@ -84,15 +92,42 @@ public class WebServiceConfig {
     	return transport.getUDDIInquiryService();
     }
     
-    private String findService(String name) throws Exception {
-    	FindService fs = new FindService();
-    	fs.getName().add(new Name(serviceName, "es"));
-        ServiceList serviceList = inquiryService().findService(fs);
-        
-        return getAccessPoint(serviceList.getServiceInfos().getServiceInfo().get(0));
+    public UDDISecurityPortType securityService() throws Exception {
+    	UDDIClient uddiClient = uddiClient();
+    	Transport transport = uddiClient.getTransport("default");
+    	return transport.getUDDISecurityService();
     }
     
-    private String getAccessPoint(ServiceInfo serviceInfo) throws Exception {
+    private AuthToken authToken() throws Exception {
+    	GetAuthToken getter = new GetAuthToken();
+    	getter.setUserID(juddiUser);
+    	getter.setCred(juddiPassword);
+    	return securityService().getAuthToken(getter);
+    }
+    
+    private String findService(String query) throws Exception {
+    	AuthToken authToken = authToken();
+    	FindService fs = new FindService();
+    	
+    	Name name = new Name();
+    	name.setValue(query);
+    	fs.getName().add(name);
+
+    	fs.setAuthInfo(authToken.getAuthInfo());
+    	
+    	FindQualifiers qualifiers = new FindQualifiers();
+    	qualifiers.getFindQualifier().add(UDDIConstants.EXACT_MATCH);
+    	fs.setFindQualifiers(qualifiers);
+    	
+        ServiceList serviceList = inquiryService().findService(fs);
+        ServiceInfo firstServiceInfo = serviceList.getServiceInfos().getServiceInfo().get(0); 
+        return getAccessPoint(
+        		firstServiceInfo,
+        		authToken
+        		);
+    }
+    
+    private String getAccessPoint(ServiceInfo serviceInfo, AuthToken authToken) throws Exception {
     	UDDIInquiryPortType inquiryService = inquiryService();
     	GetServiceDetail gsd=new GetServiceDetail();
     	
@@ -100,14 +135,14 @@ public class WebServiceConfig {
         String servicekey = serviceInfo.getServiceKey();
 
         GetServiceDetail getServiceDetail=new GetServiceDetail();
-        //getServiceDetail.setAuthInfo(authinfo);
+        getServiceDetail.setAuthInfo(authToken.getAuthInfo());
         getServiceDetail.getServiceKey().add(servicekey);
         ServiceDetail serviceDetail=inquiryService.getServiceDetail(getServiceDetail);
         BusinessService businessservice=serviceDetail.getBusinessService().get(0);
         String bindingkey = businessservice.getBindingTemplates().getBindingTemplate().get(0).getBindingKey();
 
         GetBindingDetail gbd = new GetBindingDetail();
-        //gbd.setAuthInfo(authinfo);
+        gbd.setAuthInfo(authToken.getAuthInfo());
         gbd.getBindingKey().add(bindingkey);
         BindingDetail bindingdetail=inquiryService.getBindingDetail(gbd);
         BindingTemplate bindingtemplate=bindingdetail.getBindingTemplate().get(0);
@@ -122,11 +157,11 @@ public class WebServiceConfig {
     	try {
     		url = findService(serviceName);
     	} catch(Exception ex) {
-    		url = defaultUrl; 
+    		url = ex.getMessage();
     	}
     	return url;
     }
-    
+
     @Bean
     public WebServiceTemplate webServiceTemplate() {
         WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
