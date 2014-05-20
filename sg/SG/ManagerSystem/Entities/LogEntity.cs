dacf4d5bf1d6406eae6acf2c92e3475b4f8ae7d8
Martín Candela Calabuig <rellikiox@gmail.com>
2014-05-20 20:13:38 +0200

    logs

---
+++ sg/SG/ManagerSystem/Entities/LogEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ManagerSystem.Entities
+{
+    public enum LogLevel { INFO, WARNING, ERROR, CRITICAL }
+
+    public class LogEntity : BaseEntity
+    {
+        public string message { get; set; }
+
+        public int? user_id { get; set; }
+
+        [ForeignKey("user_id")]
+        public virtual UserEntity user { get; set; }
+
+        public LogLevel level;
+    }
+}
---
+++ sg/SG/ManagerSystem/Services/LogService.cs
@@ -0,0 +1,47 @@
+﻿using ManagerSystem.DataAccess;
+using ManagerSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManagerSystem.Services
+{
+    public class LogService : BaseService
+    {
+        public LogService(UnitOfWork uow = null) : base(uow) { }
+
+        public void Log(string msg, LogLevel level)
+        {
+            LogEntity log = new LogEntity()
+            {
+                message = msg,
+                level = level,
+                user = authService.getCurrentUser()
+            };
+            unitOfWork.LogRepository.Insert(log);
+            unitOfWork.Save();
+        }
+
+        public void Info(string msg)
+        {
+            this.Log(msg, LogLevel.INFO);
+        }
+
+        public void Warning(string msg)
+        {
+            this.Log(msg, LogLevel.WARNING);
+        }
+
+        public void Error(string msg)
+        {
+            this.Log(msg, LogLevel.ERROR);
+        }
+
+        public void Critial(string msg)
+        {
+            this.Log(msg, LogLevel.CRITICAL);
+        }
+
+    }
+}
